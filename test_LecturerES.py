b49903a1794680617a19383508b85c45a7de8da7
Danial Mecja <danialmecja@gmail.com>
2021-05-19 01:43:00 +0800

    Add Supervised Learning concept

--- test_LecturerES.py
+++ test_LecturerES.py
@@ -27,6 +27,23 @@ class IntroToML(KnowledgeEngine):
         st.write("- Supervised Learning") 
         st.write("- Unsupervised Learning")
 
+class SupervisedLearning(KnowledgeEngine):
+    @Rule(InfoML(choice='Introduction'))
+    def intro_SL(self):
+        st.write("In supervised learning, we are given a data set and already know what our current output should look like, having the idea that there is a relationship between the input and output.")
+        st.write("Supervised learning problems are categorized into regression & classification problems.")
+
+    @Rule(InfoML(choice='Regression Problem'))
+    def regression_def(self):
+        st.write("In a regression problem, we are trying to predict results within a continuous output, meaning that we are trying to map input variables to some continuous function")
+        st.write("For example: Given data about the size of houses on the real estate market, try to predict their price. Price as a function of size is a continuous output, so this is a regression problem.")
+
+    @Rule(InfoML(choice='Classification Problem'))
+    def classification_def(self):
+        st.write("In a classification problem, we are trying to map input variables into discrete categories. In other words, we are trying to map input variables into discrete categories.")
+        st.write("Example: Given data about the size of houses on the real estate market, try to predict whether the house sells for more or less than the asking price. Here we are classifying the houses based on price into 2 discrete categories.")
+
+    
 
 st.title("Lecturer Expert System - Machine Learning")
 
@@ -34,15 +51,21 @@ with st.form(key='my_form'):
     name = st.text_input(label='What is your name?')
     submit_button = st.form_submit_button(label='Submit')
 if submit_button:
-    st.write(f"Hello {name}! Welcome to the Machine Learning Lecturer Expert System!")
+    st.write(f"Why hello there {name}! Welcome to the Machine Learning Lecturer Expert System!")
 
 st.write(f"What would you like to know about Machine Learning? Please choose a chapter that you would like to learn!")
 decision = st.selectbox('', ('Pick One!', "Introduction to ML", "Supervised Learning", 
-                        "Unsupervised Learning", "Model Representation", "Cost Function", "Gradient Descent"))
+                          "Unsupervised Learning", "Model Representation", "Cost Function", "Gradient Descent"))
 
 if decision == "Introduction to ML":
     intro_choice = st.selectbox("What would you like to know in Introduction to ML?", ('', "Definitions", "Examples of the Definitions", "Classification of Machine Learning"))
     engine = IntroToML()
     engine.reset()
     engine.declare(InfoML(choice=intro_choice))
+    engine.run()
+elif decision == "Supervised Learning":
+    sl_choice = st.selectbox("What would you like to know in Supervised Learning?", ('', "Introduction", "Regression Problem", "Classification Problem"))
+    engine = SupervisedLearning()
+    engine.reset()
+    engine.declare(InfoML(choice=sl_choice))
     engine.run()
